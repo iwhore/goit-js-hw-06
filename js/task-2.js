2799e3ce8c3451dba17c1f82efa94b41ea350c52
iwhore <ihorjaremko17@gmail.com>
2024-01-27 23:49:33 +0100

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,22 @@
 class Storage {
+  #items;
   constructor(items) {
-    this.items = items;
+    this.#items = items;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    this.items = this.items.filter( item => item !== itemToRemove)
+    const index = this.#items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.#items.splice(index, 1);
+    }
   }
 }
 
